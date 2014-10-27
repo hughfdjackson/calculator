72c7effd978ad06832f07c324b7a8bf15176ed72
Hugh FD Jackson <hughfdjackson@googlemail.com>
2014-10-27 20:18:43 +0000

    Episode 3. Parsing a chain of nums and + operators

--- src/Main.hs
+++ src/Main.hs
@@ -3,7 +3,19 @@ import Text.Parsec.String (Parser)
 
 
 number :: Parser Float
-number = fmap readFloat numberString
-  where readFloat = read :: String -> Float
+number = withWhitespace plainNumber
+  where plainNumber = fmap readFloat numberString
+        readFloat = read :: String -> Float
         numberString :: Parser String
         numberString = many1 digit
+
+plus :: Parser (Float -> Float -> Float)
+plus = fmap (const add) plusChar
+  where add a b = a + b
+        plusChar = char '+'
+
+chainPlus :: Parser Float
+chainPlus = chainl1 number plus
+
+withWhitespace :: Parser a -> Parser a
+withWhitespace parser = between spaces spaces parser 
