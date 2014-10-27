805a23eff689322842c3a7f1612b06c6d3ef31d0
Hugh FD Jackson <hughfdjackson@googlemail.com>
2014-10-27 20:26:34 +0000

    Episode 4. Subtraction

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,4 @@
+import Prelude hiding (subtract)
 import Text.Parsec
 import Text.Parsec.String (Parser)
 
@@ -6,16 +7,18 @@ number :: Parser Float
 number = withWhitespace plainNumber
   where plainNumber = fmap readFloat numberString
         readFloat = read :: String -> Float
-        numberString :: Parser String
         numberString = many1 digit
+        withWhitespace parser = between spaces spaces parser 
 
 plus :: Parser (Float -> Float -> Float)
 plus = fmap (const add) plusChar
   where add a b = a + b
         plusChar = char '+'
 
-chainPlus :: Parser Float
-chainPlus = chainl1 number plus
+subtract :: Parser (Float -> Float -> Float)
+subtract = fmap (const subtract) subtractChar
+  where subtract a b = a - b
+        subtractChar = char '-'
 
-withWhitespace :: Parser a -> Parser a
-withWhitespace parser = between spaces spaces parser 
+chainPlusSubtract :: Parser Float
+chainPlusSubtract = chainl1 number (plus <|> subtract) 
